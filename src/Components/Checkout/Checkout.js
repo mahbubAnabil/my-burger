0808654a58d2c1a82fcad22e7b92c339eb8a2377
Nabil <mahabubahmednabil@gmail.com>
2020-12-28 13:53:13 +0600

    Saving Orders to Database (firebase)

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+const mapStateToprops = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
+
 class Checkout extends Component {
     state = {
         values: {
@@ -24,12 +35,27 @@ class Checkout extends Component {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.props.totalPrice,
+            orderTime: new Date(),
+        }
+        axios.post("https://burger-builder-2c44e-default-rtdb.firebaseio.com/orders.json", order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+
     }
 
     render() {
         return (
             <div>
+                <h4 style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}>Payment: {this.props.totalPrice}</h4>
                 <form style={{
                     border: "1px solid grey",
                     boxShadow: "1px 1px #888888",
@@ -54,4 +80,4 @@ class Checkout extends Component {
     }
 }
 
-export default Checkout;
+export default connect(mapStateToprops)(Checkout);
