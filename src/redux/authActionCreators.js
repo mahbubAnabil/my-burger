b70fcdcf52941da452b17328f01079fac4c92098
Nabil <mahabubahmednabil@gmail.com>
2021-02-24 17:49:06 +0600

    Using Local Storage to Store token

--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -27,7 +27,26 @@ export const auth = (email, password, mode) => dispatch => {
     const API_KEY = "AIzaSyB6X1Rh2qcFHHB4_V5Y45GdYXqGoKkuPxc";
     axios.post(authUrl + API_KEY, authData)
         .then(response => {
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('userId', response.data.localId);
+            const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('expirationTime', expirationTime);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
 
         })
+}
+
+export const authCheck = () => dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        //Logout
+    } else {
+        const expirationTime = new Date(localStorage.getItem('expirationTime'));
+        if (expirationTime <= new Date()) {
+            //Logout
+        } else {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+        }
+    }
 }
